e0114ba002b41ba0eba7655c4250c983bb3b432c
Evodie-nday <ndayishimiyeevodie1@gmail.com>
2025-02-12 00:26:27 -0800

    second commit, created findAdmins function that takes users as parameter and output users and their roles

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-function countRoles(users) {
-  const roleCounts = {};
+// function countRoles(users) {
+//   const roleCounts = {};
 
-  for (let i = 0; i < users.length; i++) {
-    const role = users[i].role;
+//   for (let i = 0; i < users.length; i++) {
+//     const role = users[i].role;
 
-    if (roleCounts[role]) {
-      roleCounts[role]++;
-    } else {
-      roleCounts[role] = 1;
-    }
-  }
+//     if (roleCounts[role]) {
+//       roleCounts[role]++;
+//     } else {
+//       roleCounts[role] = 1;
+//     }
+//   }
 
-  return roleCounts;
+//   return roleCounts;
+// }
+// const users = [
+//   { name: 'Evan', role: 'admin' },
+//   { name: 'Samantha', role: 'student' },
+//   { name: 'Elsie', role: 'admin' },
+//   { name: 'Loryn', role: 'instructor' },
+//   { name: 'Lilian', role: 'student' },
+//   { name: 'Josiane', role: 'student' },
+//   { name: 'Grace', role: 'student' },
+//   { name: 'Rosine', role: 'student' },
+// ]
+// console.log(countRoles(users));
+
+function findAdmins(users) {
+  return users
+    .filter(user => user.role === 'admin')
+    // .map(user => user.name);
 }
+
+
 const users = [
   { name: 'Evan', role: 'admin' },
   { name: 'Samantha', role: 'student' },
@@ -21,7 +40,7 @@ const users = [
   { name: 'Lilian', role: 'student' },
   { name: 'Josiane', role: 'student' },
   { name: 'Grace', role: 'student' },
-  { name: 'Rosine', role: 'student' },
-]
-console.log(countRoles(users));
+  { name: 'Rosine', role: 'student' }
+];
 
+console.log(findAdmins(users));
